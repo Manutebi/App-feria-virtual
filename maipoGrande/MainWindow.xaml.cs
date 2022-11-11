5bb170cd8a0e128bebb9558fc76bf6ff112dd0c9
Manutebi <ma.aninir@duocuc.cl>
2022-11-10 21:20:50 -0300

    Se sube proyecto

---
+++ maipoGrande/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Configuration;
+using System.Data;
+using maipoGrandeDatos;
+
+namespace maipoGrande
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OracleConnection conn = null;
+
+        public MainWindow()
+        {
+            abrirConexion();
+            InitializeComponent();
+        }
+
+        private void abrirConexion()
+        {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["oracleDB"].ConnectionString;
+            conn = new OracleConnection(ConnectionString);
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de conexion");
+                throw new Exception("Error de conexion") ;
+            }
+
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OracleCommand comando = new OracleCommand("SELECT * FROM usuario WHERE EMAIL = :email AND PASSWORD = :password AND ROL_ID_ROL = 1", conn);
+
+                comando.Parameters.Add(":email", userBox.Text);
+                comando.Parameters.Add(":password", passBox.Password);
+
+                OracleDataReader lector = comando.ExecuteReader();
+
+                if (lector.Read())
+                {
+                    new Window1().Show();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("No se reconoce un usuario correcto. Vuelva a intentar");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error en la conexion");
+            }
+        }
+    }
+}
---
+++ maipoGrandeDatos/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maipoGrandeDatos
+{
+    public class Usuario
+    {
+        private int _id_usuario;
+        private string _nombre;
+        private string _apellido;
+        private string _email;
+        private string _password;
+        private int _run;
+        private int _usuario_activo;
+        private int _superuser;
+        private int _ciudad_id_ciudad;
+        private int _rol_id_rol;
+
+        public int Id_usuario { get => _id_usuario; set => _id_usuario = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string Password { get => _password; set => _password = value; }
+        public int Run { get => _run; set => _run = value; }
+        public int Usuario_activo { get => _usuario_activo; set => _usuario_activo = value; }
+        public int Superuser { get => _superuser; set => _superuser = value; }
+        public int Ciudad_id_ciudad { get => _ciudad_id_ciudad; set => _ciudad_id_ciudad = value; }
+        public int Rol_id_rol { get => _rol_id_rol; set => _rol_id_rol = value; }
+    }
+    public class Rol
+    {
+        private int _id_rol;
+        private string _n_rol;
+
+        public int Id_rol { get => _id_rol; set => _id_rol = value; }
+        public string N_rol { get => _n_rol; set => _n_rol = value; }
+    }
+ 
+}
