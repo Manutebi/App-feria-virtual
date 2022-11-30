03efd4ccbc617f374647b89f3a12de186ec183ed
Manutebi <ma.aninir@duocuc.cl>
2022-11-29 23:56:24 -0300

    Intento validaciones

---
+++ maipoGrande/Validaciones/Vusuarios.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using maipoGrandeDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using maipoGrande.Pages;
+
+namespace maipoGrande.Validaciones
+{
+    public class Vusuarios : AbstractValidator<Usuario>
+    {
+        public Vusuarios() 
+        {
+            RuleFor(p => p.Nombre)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("El usuario esta vacio.")
+                .Length(5, 10).WithMessage("El usuario debe tener entre 5 y 10 caracteres")
+                .Must(LestrasValidas).WithMessage("El usuario contiene caracteres invalidos");
+                
+        }
+
+        protected bool LestrasValidas(string Nombre)
+        {
+            Nombre = Nombre.Replace(" ", "");
+            Nombre = Nombre.Replace("-", "");
+            return Nombre.All(Char.IsLetter);
+        }
+    }
+}
--- maipoGrande/Window1.xaml.cs
+++ maipoGrande/Window1.xaml.cs
@@ -171,37 +171,11 @@ namespace maipoGrande
         }
 
 
-        //Theme Code ========================>
-        public bool IsDarkTheme { get; set; }
-        private readonly PaletteHelper paletteHelper = new PaletteHelper();
-        //===================================>
-
       
 
-        private void toggleTheme(object sender, RoutedEventArgs e)
-        {
-            //Theme Code ========================>
-            ITheme theme = paletteHelper.GetTheme();
-            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
-            {
-                IsDarkTheme = false;
-                theme.SetBaseTheme(Theme.Light);
-            }
-            else
-            {
-                IsDarkTheme = true;
-                theme.SetBaseTheme(Theme.Dark);
-            }
+       
 
-            paletteHelper.SetTheme(theme);
-            //===================================>
-        }
-
-        private void exitApp(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
-
+     
 
     }
 }
